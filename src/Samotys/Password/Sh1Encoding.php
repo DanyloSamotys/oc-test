53519a8eccaea5a21a876eab98492ef8fbd6f10a
Danylo Samotys <danylo.samotys@miquido.com>
2015-12-17 08:38:56 +0100

    Added new encoding method.

---
+++ src/Samotys/Password/Sh1Encoding.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the "PasswordEncoder" project.
+ * @author Jakub Kanclerz <kuba.kanclerz@creativestyle.pl>
+ * Feel free to contact me
+ */
+
+namespace Samotys\Password;
+
+use Jkan\Component\Password\EncodingMethod;
+
+class Sh1Encoding implements EncodingMethod
+{
+
+    public function encode($plainText)
+    {
+        return sha1($plainText);
+    }
+}
